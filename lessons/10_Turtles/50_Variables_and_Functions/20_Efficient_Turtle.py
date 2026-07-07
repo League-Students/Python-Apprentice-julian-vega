47f8a0e4eb10ce2c7ae145ccc760d30e38234c9b
League Student <student@jointheleague.org>
2026-07-07 12:25:17 -0700

    Automated commit

--- lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
+++ lessons/10_Turtles/50_Variables_and_Functions/20_Efficient_Turtle.py
@@ -16,7 +16,7 @@ tina = turtle.Turtle()                   # Create a turtle named tina
 tina.shape('turtle')                     # Set the shape of the turtle to a turtle
 tina.speed(2)
 
-                            # Move at a moderate speed, not too fast.
+sp==                            # Move at a moderate speed, not too fast.
 
 def draw_polygon(sides):
 
