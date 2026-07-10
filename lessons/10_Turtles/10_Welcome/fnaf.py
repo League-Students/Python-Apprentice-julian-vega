0eb0e469750a4fe5ea26eb25e005e7fa87a1a127
League Student <student@jointheleague.org>
2026-07-10 13:15:19 -0700

    Automated commit

--- lessons/10_Turtles/10_Welcome/fnaf.py
+++ lessons/10_Turtles/10_Welcome/fnaf.py
@@ -25,15 +25,19 @@ def open_cam_1():
 def open_cam_2():
   print("cam 2 open")
   screen.bgcolor(cam_colors[1]) 
+  show_animatronics()
 def open_cam_3():
   print("cam 3 open")
-  screen.bgcolor(cam_colors[2]) 
+  screen.bgcolor(cam_colors[2])
+  show_animatronics() 
 def open_cam_4():
   print("cam 4 open")
-  screen.bgcolor(cam_colors[3]) 
+  screen.bgcolor(cam_colors[3])
+  show_animatronics() 
 def open_cam_5():
   print("cam 5 open")
-  screen.bgcolor(cam_colors[4]) 
+  screen.bgcolor(cam_colors[4])
+  show_animatronics() 
 def exit_cam():
   print("cam exited")
   screen.bgcolor("yellow")
